a9a887a649eb37887dfc2e38541e57954dc3c00f
Ghiyosov <154344895+Ghiyosov@users.noreply.github.com>
2024-01-31 18:10:47 +0500

    Update Employee.cs

--- ClassTask/Employee.cs
+++ ClassTask/Employee.cs
@@ -10,7 +10,7 @@ public class Employee
     public int Age { get; set; }
     public string PhoneNumber { get; set; }
     public decimal Salary { get; set; }
-    public Employee(){};
+    public Employee(){}
     public Employee(string name, string surname)
     {
         FirstName = name;
